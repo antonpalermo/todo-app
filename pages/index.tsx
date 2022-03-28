d2a4d2940422dbc3e0f5229a29cb9d13dcee87b8
Anton Palermo <ANPBusinessmail@gmail.com>
2022-03-28 14:34:43 +0800

    initialize prisma with next-auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
+import { GetServerSideProps, GetServerSidePropsContext } from 'next'
+import { getSession } from 'next-auth/react'
 import React from 'react'
 
 const Home = () => {
   return <div>home</div>
+}
+
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: GetServerSidePropsContext
+) => {
+  const session = await getSession(ctx)
+
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination:
+          '/api/auth/signin?callbackUrl=' + encodeURIComponent(ctx.resolvedUrl)
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
 }
 
 export default Home
